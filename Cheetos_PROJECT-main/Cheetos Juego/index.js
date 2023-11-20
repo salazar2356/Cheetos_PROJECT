13149e37b57a60a2e6d02c61ecacce0ff82d6aac
Kazura-Collins <98463668+Kazura-Collins@users.noreply.github.com>
2023-11-19 20:42:04 -0500

    Intento #1 del botón
    Ya había una base, pero puse la función disparar y con suerte el socket la recibe desde el phone.js al sketch.js
    No puedo probarlo sin el joystick, al parecer me pide puerto COM4.

---
+++ Cheetos_PROJECT-main/Cheetos Juego/index.js
@@ -0,0 +1,69 @@
+const express = require("express"); 
+
+const expressApp = express();
+const PORT = 5050;
+
+const httpServer = expressApp.listen(PORT);
+const {Server} = require("socket.io");
+const ioServer = new Server(httpServer);
+
+const staticController = express.static('public-controller');
+const staticDisplay = express.static('public-display');
+
+expressApp.use('/controller', staticController);
+expressApp.use('/display',staticDisplay);
+
+ioServer.on('connection', (socket) => {
+    //Configurar un manejador de eventos para al evento "confimation"
+    socket.on('confirmation', (data) => {
+        socket.broadcast.emit('confirmation', data)
+    })
+})
+
+
+
+
+// Import de SerialPort package
+const {
+    SerialPort,
+    ReadlineParser
+} = require('serialport');
+
+
+SerialPort.list().then((ports) => {
+    console.log('Available ports:');
+    ports.forEach((port) => {
+      console.log(port.path);
+    });
+  });
+
+// Set the rules for the serial communication
+
+// Opens a port
+const port = new SerialPort({
+    path: 'COM4',
+    baudRate: 9600
+});
+
+port.on('error', (err) => {
+    console.error('Error en el puerto serial:', err.message);
+  });
+
+
+//--------------------------------------- 1- Read without parsing
+
+// Read data from Serial Buffer
+
+// port.on('data', (data) => {
+//     console.log("-----",data);
+// })
+
+//--------------------------------------- 2- 4- Reading after parsing
+
+const parser = port.pipe(new ReadlineParser);
+
+parser.on('data', (data) => {
+    console.log(data);
+})
+
+//--------------------------------------- 3- From String to Integer
